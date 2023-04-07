8d1f6b7c4b01a070df984f156d5df3e36d1bcf24
ilysharusher <illyaetoilysha@gmail.com>
2023-04-07 17:20:49 +0300

    some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     darkMode: false,
     theme: {
-        extend: {},
+        container: {
+            center: true,
+        },
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1127px',
+        },
+        maxWidth: {
+            DEFAULT: '100%',
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1127px',
+        },
+        extend: {}
     },
     plugins: [],
 }
