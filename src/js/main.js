591b9127f9f65e3db296c191cf6c09a19d091325
ilysharusher <illyaetoilysha@gmail.com>
2023-04-20 16:49:01 +0300

    Day 9 in progress

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,63 @@ let app = new Vue({
     el: "#article",
     data: {
         products: [
-            {id: 1, title: "Buryak 1", short_text: "About Buryak 1", image: 'images/buryak/buryak1.jpeg', desc: "Full desc"},
-            {id: 2, title: "Buryak 2", short_text: "About Buryak 2", image: 'images/buryak/buryak2.jpg', desc: "Full desc"},
-            {id: 3, title: "Buryak 3", short_text: "About Buryak 3", image: 'images/buryak/buryak3.jpeg', desc: "Full desc"},
-            {id: 4, title: "Buryak 4", short_text: "About Buryak 4", image: 'images/buryak/buryak4.jpg', desc: "Full desc"},
-            {id: 5, title: "Buryak 5", short_text: "About Buryak 5", image: 'images/buryak/buryak5.jpg', desc: "Full desc"}
-        ]
+            {
+                id: 1,
+                title: "Buryak 1",
+                short_text: "About Buryak 1",
+                image: 'images/buryak/buryak1.jpeg',
+                desc: "Full desc"
+            },
+            {
+                id: 2,
+                title: "Buryak 2",
+                short_text: "About Buryak 2",
+                image: 'images/buryak/buryak2.jpg',
+                desc: "Full desc"
+            },
+            {
+                id: 3,
+                title: "Buryak 3",
+                short_text: "About Buryak 3",
+                image: 'images/buryak/buryak3.jpeg',
+                desc: "Full desc"
+            },
+            {
+                id: 4,
+                title: "Buryak 4",
+                short_text: "About Buryak 4",
+                image: 'images/buryak/buryak4.jpg',
+                desc: "Full desc"
+            },
+            {
+                id: 5,
+                title: "Buryak 5",
+                short_text: "About Buryak 5",
+                image: 'images/buryak/buryak5.jpg',
+                desc: "Full desc"
+            }
+        ],
+        product: [],
+        butVisible: 0
     },
     mounted: function () {
-        console.log(window.localStorage.getItem('prod'));
+        this.getProduct();
     },
     methods: {
-        addItem: function (id) {
-            window.localStorage.setItem('prod', id)
+        getProduct: function () {
+            if (window.location.hash) {
+                let id = window.location.hash.replace('#', '');
+                console.log(id);
+                if (this.products && this.products.length > 0) {
+                    for (let i in this.products) {
+                        console.log(this.products[i].id, id);
+                        if (this.products[i] && this.products[i].id && id == this.products[i].id) {
+                            console.log(this.products[i]);
+                            this.product = this.products[i];
+                        }
+                    }
+                }
+            }
         }
     }
 });
