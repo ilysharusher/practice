f0feb687ebfe907c56cc60940c86ba4ea733052c
ilysharusher <illyaetoilysha@gmail.com>
2023-04-24 15:08:04 +0300

    Add work with cache

--- sw.js
+++ sw.js
@@ -1,11 +1,28 @@
 self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
+    event.waitUntil(
+        caches.open('static')
+            .then(function (cache) {
+                console.log('precaching');
+                cache.add('src/index.html');
+                cache.add('src/');
+            })
+    );
 });
+
 self.addEventListener('activate', function (event) {
     console.log('[Service Worker] Activating Service Worker ...', event);
     return self.clients.claim();
 });
+
 self.addEventListener('fetch', function (event) {
-    console.log('[Service Worker] Fetching something ....', event);
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if (response)
+                    return response;
+                else
+                    return fetch(event.request);
+            })
+    );
 });
